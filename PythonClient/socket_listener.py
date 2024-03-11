9e3d6e3d0da35c7703ac38269f6eccb5289314c5
Ho Lyun Jeong <holyun.lucas.jeong@gmail.com>
2024-03-11 01:41:44 -0700

    receive_packet and buildPacket todo

--- PythonClient/socket_listener.py
+++ PythonClient/socket_listener.py
@@ -3,6 +3,11 @@ import json
 import struct
 import packets
 # b'\x00w\x04holyv\x00+Freeciv-3.0-network year32 plrculture32 pingfix researchclr cityculture32 rsdesc32 obsinv\x00-msys2\x00\x00\x00\x00\x03\x00\x00\x00\x00\x00\x00\x00\n'
+
+# todo
+def buildPacket(pid, payload):
+  return {}
+
 class SocketClient:
     def __init__(self, server_ip, server_port):
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,6 +16,8 @@ class SocketClient:
         self._partial_packet = b''
         self._partial_packet_length = 0
         print("Created socket")
+
+        # todo: delete this as this should be handled by the modified client...
         try:
             self.client_socket.connect((server_ip, server_port))
             print("Connected!")
@@ -52,4 +59,12 @@ class SocketClient:
             self._partial_packet = b''
             self._partial_packet_length = 0
 
+    def receive_packet():
+        data = client.recv(2)
+        packet_len = int.from_bytes(data, byteorder='big')
 
+        data = client.recv(packet_len - 2)
+        packet_type = int.from_bytes(data[:2], byteorder='big')
+        packet_payload = data[2:]
+
+        return buildPacket(packet_type, packet_payload)
