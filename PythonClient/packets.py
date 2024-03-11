626ca104f3567e26bd2a229781c42a1d14918958
Colin Jerome McCarson <colin.jerome.mccarson@gmail.com>
2024-03-11 03:34:44 -0700

    Factory method boilerplate. Some subclasses finished, but pending Adit finalization for all construction.

--- PythonClient/packets.py
+++ PythonClient/packets.py
@@ -1,5 +1,7 @@
 import socket
 import struct
+import enum
+import numpy as np
 
 
 class Packet:
@@ -45,12 +47,11 @@ class Packet:
                 data += arg.encode() + b'\0'
             elif self.types[field] == 'int':
                 data += struct.pack('>I', arg)
+            elif self.types[field] == 'array':
+                pass # TODO
 
         data = struct.pack('>H', len(data) + 2) + data
         return data
-
-    def decode(self, message):
-        pass  # TODO str, int, array
 
 # TODO subclass packet for every packet type we need!
 # TODO make PacketFactory??
@@ -68,7 +69,7 @@ class HelloReplyPacket(Packet):  # 1
 class MapPacket(Packet):  # 2
     def __init__(self):
         super().__init__(2)
-        self._add_field('map', 25600, 'array')
+        self._add_field('map', 102400, 'array')
 
 class UnitInfoPacket(Packet):  # 3
     def __init__(self):
@@ -80,7 +81,7 @@ class CivInfoPacket(Packet):  # 4
     def __init__(self):
         super().__init__(4)
         self._add_field('nation_tag', 20, 'int')
-        self._add_field('')
+        # TODO add fields
 
 class CityInfoPacket(Packet):  # 5
     def __init__(self):
@@ -95,6 +96,7 @@ class ActionPacket(Packet):  # 6
         super().__init__(6)
         self._add_field('action', 100, 'str')
         self._add_field('action_specifiers', 25000, 'str')
+        # TODO maybe add more?
 
 class ActionReplyPacket(Packet):  # 7
     def __init__(self):
@@ -104,9 +106,76 @@ class ActionReplyPacket(Packet):  # 7
 class TurnBeginPacket(Packet):  # 8
     def __init__(self):
         super().__init__(8)
-        self._add_field('turn_begin', 100, 'str')
+        self._add_field('turn_begin', 1000, 'int')
 
-class TurnEndPacket(Packet):
+class TurnEndPacket(Packet):  # 9
     def __init__(self):
         super().__init__(9)
         self._add_field('turn_end', 100, 'str')
+
+class CompletedStateTransferPacket(Packet):  # 10
+    def __init__(self):
+        super().__init__(10)
+        self._add_field('done', 100, 'str')
+
+class PacketEnum(enum.Enum):
+    Hello = 0
+    HelloReply = 1
+    Map = 2
+    UnitInfo = 3
+    CivInfo = 4
+    CityInfo = 5
+    Action = 6
+    ActionReply = 7
+    TurnBegin = 8
+    TurnEnd = 9
+    CompletedStateTransfer = 10
+
+class PacketFactory(Packet):
+    def make_packet(self, packet_type, bytestream):
+        if packet_type == PacketEnum.Hello.value:
+            hello = HelloPacket()
+            hello.set_content('greeting', bytestream.decode('ascii'))
+            return hello
+        elif packet_type == PacketEnum.HelloReply.value:
+            hello_reply = HelloPacket()
+            hello_reply.set_content('greeting', bytestream.decode('ascii'))
+            return hello_reply
+        elif packet_type == PacketEnum.Map.value:
+            mapp = MapPacket()
+            # TODO size? maybe smaller, maybe comes in 2 packets.
+            mapp.set_content('map', np.frombuffer(bytestream, dtype=np.int32))
+            return mapp
+        elif packet_type == PacketEnum.UnitInfo.value:  # TODO finish all fields
+            unit_info = UnitInfoPacket()
+            unit_info.set_content('unit_id', int.from_bytes(bytestream, byteorder='big', signed=False))  # TODO double check endianness
+            return unit_info
+        elif packet_type == PacketEnum.CivInfo.value:  # TODO finish all fields
+            civ_info = CivInfoPacket()
+            civ_info.set_content('nation_tag', int.from_bytes(bytestream, byteorder='big', signed=False))
+            return civ_info
+        elif packet_type == PacketEnum.CityInfo.value:  # TODO finish all fields
+            city_info = CityInfoPacket()
+            city_info.set_content('city_name', bytestream.decode('ascii'))
+            return city_info
+        elif packet_type == PacketEnum.Action.value:  # TODO Finish all fields
+            action = ActionPacket()
+            action.set_content('action', bytestream.decode('ascii'))
+            return action
+        elif packet_type == PacketEnum.ActionReply.value:
+            action_reply = ActionReplyPacket()
+            action_reply.set_content('action', bytestream.decode('ascii'))
+            return action_reply
+        elif packet_type == PacketEnum.TurnBegin.value:
+            turn_begin = TurnBeginPacket()
+            turn_begin.set_content('turn_begin', int.from_bytes(bytestream, byteorder='big', signed=False))
+            return turn_begin
+        elif packet_type == PacketEnum.TurnEnd.value:
+            turn_end = TurnEndPacket()
+            turn_end.set_content('turn_end', bytestream.decode('ascii'))
+            return turn_end
+        elif packet_type == PacketEnum.CompletedStateTransfer:
+            done = CompletedStateTransferPacket()
+            done.set_content('done', bytestream.decode('ascii'))
+        else:
+            raise ValueError("Unknown packet type")
