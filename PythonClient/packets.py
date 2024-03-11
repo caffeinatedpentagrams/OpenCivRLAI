3715ac31648e4240eed5ed48419916f8374ca8f9
Ho Lyun Jeong <holyun.lucas.jeong@gmail.com>
2024-03-11 04:49:34 -0700

    remove length header

--- PythonClient/packets.py
+++ PythonClient/packets.py
@@ -133,10 +133,12 @@ class PacketEnum(enum.Enum):
     CompletedStateTransfer = 10
 
 class PacketFactory:
+    # the payload should be passed in
+    # i.e. the 2-byte packet length field should be removed
     def __init__(self, bytestream):
         self.bytestream = bytestream
-        self.packet_type = int.from_bytes(self.bytestream[2 : 4], byteorder='big')
-        self.idx = 4
+        self.packet_type = int.from_bytes(self.bytestream[:2], byteorder='big')
+        self.idx = 2
 
     def get_str(self):
         cut_idx = self.bytestream.find(b'\0', self.idx)
@@ -245,7 +247,7 @@ def test(bytestream):
     packet = PacketFactory(bytestream).make_packet()
     for field in packet.field_names:
         print(f'{field}: {packet.content[field]}')
-    encoded = packet.encode()
+    encoded = packet.encode()[2:] # remove the packet length field before comparison
     if encoded == bytestream:
         print('encoded matches bytestream')
         print(f'\tgot: {encoded}')
@@ -257,34 +259,34 @@ def test(bytestream):
 
 if __name__ == '__main__':
     # hello: 'hello'
-    test(b'\x00\x0a\x00\x00hello\x00')
+    test(b'\x00\x00hello\x00')
 
     # hello reply: 'helloreply'
-    test(b'\x00\x0f\x00\x01helloreply\x00')
+    test(b'\x00\x01helloreply\x00')
 
     # map: [1 2 3]
-    test(b'\x00\x14\x00\x02\x00\x00\x00\x03\x00\x00\x00\x01\x00\x00\x00\x02\x00\x00\x00\x03')
+    test(b'\x00\x02\x00\x00\x00\x03\x00\x00\x00\x01\x00\x00\x00\x02\x00\x00\x00\x03')
 
     # unit info: id = 10
-    test(b'\x00\x08\x00\x03\x00\x00\x00\x0a')
+    test(b'\x00\x03\x00\x00\x00\x0a')
 
     # civ info: nation_tag = 7
-    test(b'\x00\x08\x00\x04\x00\x00\x00\x07')
+    test(b'\x00\x04\x00\x00\x00\x07')
 
     # city info: city_name = 'city', pop = 12, owned_by = 'me'
-    test(b'\x00\x10\x00\x05city\x00\x00\x00\x00\x0cme\x00')
+    test(b'\x00\x05city\x00\x00\x00\x00\x0cme\x00')
 
     # action: action = 'do smth', action_specifiers = 'magestically and philanthropically'
-    test(b'\x00\x2f\x00\x06do smth\x00magestically and philanthropically\x00')
+    test(b'\x00\x06do smth\x00magestically and philanthropically\x00')
 
     # action reply: action = 'pray'
-    test(b'\x00\x09\x00\x07pray\x00')
+    test(b'\x00\x07pray\x00')
 
     # turn begin: 2
-    test(b'\x00\x08\x00\x08\x00\x00\x00\x02')
+    test(b'\x00\x08\x00\x00\x00\x02')
 
     # turn end: '9'
-    test(b'\x00\x06\x00\x099\x00')
+    test(b'\x00\x099\x00')
 
     # completed state transfer: done = 'yea its done'
-    test(b'\x00\x11\x00\x0ayea its done\x00')
+    test(b'\x00\x0ayea its done\x00')
