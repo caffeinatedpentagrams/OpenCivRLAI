866359255621223e8c21089ea710770b97b13e7e
Colin Jerome McCarson <colin.jerome.mccarson@gmail.com>
2024-03-10 22:29:59 -0700

    Fixed encode

--- PythonClient/packets.py
+++ PythonClient/packets.py
@@ -1,18 +1,19 @@
 import socket
 import struct
 
+
 class Packet:
     def __init__(self, packid):
         self.packid = packid
         self.maxlens = {}
-        self.field_names = []  # Gets the ordering! NEVER ACCESS THE DICT; GET THE NAMES FROM HERE THEN ACCESS THE DICT
-        self.field_dict = {}
-        # Indexes correspond; ordering matters.
+        self.field_names = []  # Contains the right ordering! NEVER ACCESS THE DICT WITH .keys(); GET THE NAMES FROM
+        # HERE THEN ACCESS THE DICT
+        self.content = {}
 
     def add_field(self, field_name, maxlen):
         self.field_names.append(field_name)
         self.maxlens[field_name] = maxlen
-        self.field_dict[field_name] = ""
+        self.content[field_name] = ""
 
     def set_content(self, field_name, content):
         if type(content) == str:
@@ -21,17 +22,16 @@ class Packet:
         elif type(content) == int:
             if (content.bit_length() + 7) // 8 > self.maxlens[field_name]:
                 raise ValueError(f"{field_name} exceeds maximum length!")
-        self.field_dict[field_name] = content
+        self.content[field_name] = content
 
     def encode(self):
         data = struct.pack('>B', self.packid)
-        for arg in self.contents:
-            argtype = str(type(arg))
+        for field in self.field_names:
+            arg = self.content[field]
 
-            if 'str' in argtype:
+            if type(arg) == str:
                 data += arg.encode() + b'\0'
-
-            elif 'int' in argtype:
+            elif type(arg) == int:
                 data += struct.pack('>I', arg)
 
         data = struct.pack('>H', len(data) + 2) + data
